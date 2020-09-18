b62cfd936736d648b4fdd0c091603b673ec84444
zer0cache <ssfaleads@gmail.com>
2020-09-18 09:32:08 -0400

    .

--- bot.js
+++ bot.js
@@ -5,32 +5,36 @@ let _ = require('lodash');
 var numeral = require("numeral");
 require("dotenv").config();
 
-try {
-  let Bot = new TwitterBot({
-    consumer_key: process.env.TWITTER_API_KEY,
-    consumer_secret: process.env.TWITTER_API_KEY,
-    access_token: process.env.TWITTER_ACCESS_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_SECRET,
-  });
+const sendTweet = async () => {
+  try {
+    console.log("Running bot..");
+    let Bot = new TwitterBot({
+      consumer_key: process.env.TWITTER_API_KEY,
+      consumer_secret: process.env.TWITTER_API_KEY,
+      access_token: process.env.TWITTER_ACCESS_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+    });
 
-  let cgClient = new CoinGecko();
+    let cgClient = new CoinGecko();
 
-  cgClient.coins
-    .fetch("uniswap", {
-      market_data: true,
-      tickers: false,
-      community_data: false,
-      developer_data: false,
-      localization: false,
-      sparkline: false,
-    })
-    .then((res) => {
-      const dollarValue = _.toNumber(res.data.market_data.current_price) * 400;
+    const res = await cgClient.coins
+      .fetch("uniswap", {
+        market_data: true,
+        tickers: false,
+        community_data: false,
+        developer_data: false,
+        localization: false,
+        sparkline: false,
+      });
+      const dollarValue =
+        _.toNumber(res.data.market_data.current_price) * 400;
       const valueString = numeral(dollarValue).format("$0,0");
 
-      Bot.tweet(`${valueString} \n #UNISWAP $UNI`);
-    });
-} catch (err) {
-  console.log('ERROR');
-  console.log(err);
+      await Bot.tweet(`${valueString} \n #UNISWAP $UNI`);
+  } catch (err) {
+    console.log("ERROR");
+    console.log(err);
+  }
 }
+
+sendTweet();
