53cd478eeb047f2b9f99b4be8de0a13eb2e3e9b5
zer0cache <ssfaleads@gmail.com>
2020-09-18 09:29:28 -0400

    .

--- bot.js
+++ bot.js
@@ -1,23 +1,31 @@
 let Twit = require('twit');
 let TwitterBot = require('node-twitterbot').TwitterBot;
+let CoinGecko = require('coingecko-api');
+let _ = require('lodash');
+var numeral = require("numeral");
+require("dotenv").config();
+
 let Bot = new TwitterBot({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_KEY,
+  access_token: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
 });
-let phraseArray = [
-  "hey twitter",
-  "im tweeting",
-  "tweet tweet",
-  "tweetstorm time... 1/22",
-  "plz RT v important",
-  "delete ur account",
-  "it me",
-  "same"
-];
-function chooseRandom(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-let phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
-Bot.tweet(phrase);
+
+let cgClient = new CoinGecko();
+
+ cgClient.coins
+   .fetch("uniswap", {
+     market_data: true,
+     tickers: false,
+     community_data: false,
+     developer_data: false,
+     localization: false,
+     sparkline: false,
+   })
+   .then((res) => {
+     const dollarValue = _.toNumber(res.data.market_data.current_price) * 400;
+     const valueString = numeral(dollarValue).format("$0,0");
+
+     Bot.tweet(`${valueString} \n #UNISWAP $UNI`);
+   });
