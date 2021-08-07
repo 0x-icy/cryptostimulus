7958208a7aca449cdd21e7e13cd0593b2fe70119
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-08-07 00:48:19 -0400

    .

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ const sendTweet = async () => {
       console.log("valueString: ", valueString);
       T.post(
         "statuses/update",
-        { status: `${valueString} \n Sponsored by https://nftolio.xyz (NFT Portfolio Tracker) \n #uniswap $uni #nft` },
+        { status: `${valueString} \n\nSponsored by https://nftolio.xyz (NFT Portfolio Tracker)\n #uniswap $uni #nft` },
         function (err, data, response) {
           console.log(data);
         }
